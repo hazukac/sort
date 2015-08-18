19739b0728812cec75c329608a3d46c2a67445a6
otiai10 <otiai10@gmail.com>
2015-08-18 13:37:24 +0900

    Improve bubble

--- bubble/bubble.go/bubble.go
+++ bubble/bubble.go/bubble.go
@@ -3,7 +3,7 @@ package bubble
 // Sort implement bubble-sort algorithm.
 func Sort(arr []int) []int {
 	for i := 0; i < len(arr); i++ { // *N
-		for j := 0; j < len(arr)-1; j++ { // *(N-1)
+		for j := 0; j < len(arr)-(i+1); j++ { // *(N-1)
 			a, b := arr[j], arr[j+1]
 			if a > b {
 				arr[j], arr[j+1] = b, a
